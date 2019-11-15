891612b27b59c81b76dbf4a69fd3ea79bfa638aa
davidhennig <davidjhennig@gmail.com>
2019-11-15 14:02:21 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+import CharacterCard from "./components/CharacterCard";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SearchForm from "./components/SearchForm.js";
+import axios from "axios";
 
+const WrapperDiv = styled.div`
+  h1 {
+    color: limegreen;
+  }
+  h2 {
+    font-family: Comic Sans MS;
+  }
+`;
 
 export default function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setSearchResults(response.data.results);
+        console.log(response.data.results);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <main>
-      <Header />
-    </main>
+    <WrapperDiv>
+      <main>
+        <Header />
+        <Route
+          exact
+          path="/"
+          render={props => <CharacterList {...props} list={searchResults} />}
+        />
+        <Route path="/char/:id" component={CharacterCard} />
+        <Route
+          exact
+          path="/"
+          render={props => <SearchForm {...props} list={searchResults} />}
+        />
+      </main>
+    </WrapperDiv>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterList from "./CharacterList";
 
 export default function SearchForm() {
- 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const results = CharacterList.filter(character =>
+      character.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <div className="App">
+        <form>
+          <label htmlFor="name">Search:</label>
+          <input
+            id="name"
+            type="text"
+            name="textfield"
+            placeholder="Search"
+            onChange={handleChange}
+            value={searchTerm}
+          />
+        </form>
+        <div className="character-list">
+          <ul>
+            {searchResults.map(character => (
+              <li key={character}>{character}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      );
     </section>
   );
 }
